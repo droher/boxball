f4225000af77001864d3285fd19493d01f3f080c
David Roher <david.roher@gmail.com>
2020-05-23 21:18:23 -0400

    fix test

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -12,7 +12,7 @@ os.chdir(Path("/tmp/boxball"))
 
 class TestSchemas:
     def test_schemas_compile(self):
-        assert len(retrosheet_metadata.tables) == 16
+        assert len(retrosheet_metadata.tables) == 17
         assert len(baseballdatabank_metadata.tables) == 27
 
 
